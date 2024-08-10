dd207c21941d2b1d06d4a92a47b2041fe39a1fb7
Molina-Q <quentinmolina1@gmail.com>
2024-08-10 16:43:06 +0200

    chore: Refactor PersonaTable component to include sorting functionality

--- src/components/PersonaTable.tsx
+++ src/components/PersonaTable.tsx
@@ -1,15 +1,53 @@
-type Props = {
-    persona: {
-        name: string,
-        arcana: string
-    }
+import { useState } from 'react';
+import PersonaData from '../assets/PersonaData.json';
+import { getElementIcon } from '../utils/getElementIcon';
+
+interface Persona {
+    arcana: string;
+    inherits: string;
+    lvl: number;
 }
 
-export default function PersonaTable({ persona }: Props) {
+export default function PersonaTable() {
+
+    const [sortCriteria, setSortCriteria] = useState<string>('name');
+    const personas: { [key: string]: Persona } = PersonaData;
+
+    const sortedPersonas = Object.entries(personas).sort(([nameA, detailsA], [nameB, detailsB]) => {
+        switch (sortCriteria) {
+            case 'lvl':
+                return detailsA.lvl - detailsB.lvl;
+            case 'arcana':
+                return detailsA.arcana.localeCompare(detailsB.arcana);
+            case 'inherits':
+                return detailsA.inherits.localeCompare(detailsB.inherits);
+            case 'name':
+            default:
+                return nameA.localeCompare(nameB);
+        }
+    });
+
     return (
-        <div className='table-row'>
-            <p className='table-item'>{persona.name}</p>
-            <p className='table-item'>{persona.arcana}</p>
-        </div>
+        <>
+            <div className="sort-controls">
+                <label>Sort by: </label>
+                <select onChange={(e) => setSortCriteria(e.target.value)}>
+                    <option value="name">Name</option>
+                    <option value="arcana">Arcana</option>
+                    <option value="lvl">Level</option>
+                    <option value="inherits">Inherits</option>
+                </select>
+            </div>
+            {
+                sortedPersonas.map(([name, details]) => (
+                    <div className='table-row' key={name}>
+                        <figure className='table-item'><img src={getElementIcon(details.inherits)} alt="element icon" /></figure>
+                        <p className='table-item'>{name}</p>
+                        <p className='table-item'>{details.lvl}</p>
+                        <p className='table-item'>{details.arcana}</p>
+                    </div>
+                ))
+            }
+        </>
     )
 }
