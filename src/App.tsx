7d359a1306d0909d7a0afd87ccd16f235f55f461
Molina-Q <quentinmolina1@gmail.com>
2024-08-11 17:34:06 +0200

    chore: Update App.tsx to use react-router-dom for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
-import './App.css'
-import PersonaData from './assets/PersonaData.json'
-import PersonaTable from './components/PersonaTable';
-
-interface Persona {
-  name: string,
-  arcana: string
-}
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ListPersonae from './pages/ListPersonae';
+import DetailsPersona from './pages/DetailsPersona';
 
 function App() {
-
-  const personaeUsableData = {
-    names: Object.keys(PersonaData),
-    arcana: Object.values(PersonaData).map(persona => persona.arcana)
-  };
-
-  console.log(personaeUsableData.arcana);
-  
   return (
-    <>
-      <h1>Personae</h1>
-      
-      <div >
-        <div className='table-header'>
-          <p className='table-item'>name</p>
-          <p className='table-item'>arcana</p>
-        </div>
-        {PersonaData.map((persona: Persona) => (
-          <PersonaTable persona={persona} key={persona.name} />
-        ))}
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ListPersonae />} />
+        <Route path="/persona/:persona" element={<DetailsPersona />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
