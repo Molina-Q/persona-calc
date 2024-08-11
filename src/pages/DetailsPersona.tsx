c89708f88300fcc14cabbf505f0e773fb44b3338
Molina-Q <quentinmolina1@gmail.com>
2024-08-11 17:34:17 +0200

    feat: Add DetailsPersona component for displaying persona details

---
+++ src/pages/DetailsPersona.tsx
@@ -0,0 +1,35 @@
+import { useParams } from "react-router-dom"
+import PersonaData from '../assets/PersonaData.json';
+
+interface Persona {
+    arcana: string;
+    inherits: string;
+    lvl: number;
+}
+
+export default function DetailsPersona() {
+
+    const params = useParams()
+    const persona = params.persona;
+
+    const personaType: { [key: string]: Persona } = PersonaData;
+
+    const personaData = Object.entries(personaType).find(([name]) => name === persona);
+    const name = personaData && personaData[0];
+    const details = personaData && personaData[1];
+
+    console.log(personaData);
+
+    return (
+        <div>
+            <h1>DetailsPersona</h1>
+
+            <div>
+                <p>{name}</p>
+                {details && <p>{details.arcana}</p>}
+            </div>
+
+
+        </div>
+    )
+}
