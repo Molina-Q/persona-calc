257936c02d62f161ddfb6f8e0542a14752c03006
Molina-Q <quentinmolina1@gmail.com>
2024-08-15 15:45:59 +0200

    feat: Display persona details in DetailsPersona component

--- src/pages/DetailsPersona.tsx
+++ src/pages/DetailsPersona.tsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom"
 import PersonaData from '../assets/PersonaData.json';
+import BackArrow from "../components/BackArrow";
 
 interface Persona {
     arcana: string;
@@ -8,24 +9,27 @@ interface Persona {
 }
 
 export default function DetailsPersona() {
-
     const params = useParams()
     const persona = params.persona;
 
     const personaType: { [key: string]: Persona } = PersonaData;
 
     const personaData = Object.entries(personaType).find(([name]) => name === persona);
+
+    if (!personaData) return <h1>Persona not found</h1>
     
-    const name = personaData && personaData[0];
-    const details = personaData && personaData[1];
+    const [ name, details ] = personaData;
 
     return (
         <div>
-            <h1>DetailsPersona</h1>
+            <BackArrow />
+            <h1>{name}</h1>
 
             <div>
-                <p>{name}</p>
-                {details && <p>{details.arcana}</p>}
+                <div className="personaTitle">
+                    <p>level {details.lvl}</p>
+                </div>
+                <p>{details.arcana}</p>
             </div>
 
 
